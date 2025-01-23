6823318eb21e357059feb912f8737912e029425f
milandhangar123 <ms8755301254@gmail.com>
2025-01-23 14:00:57 +0530

    fix: resolved cors issue

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 dotenv.config();
 
@@ -16,6 +17,7 @@ const connectDB = require("./config/dbconfig");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 connectDB();
 
